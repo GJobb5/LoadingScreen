23f21d974519447786cbf51ee393eade39108d9e
GJobb5 <poohg59@gmail.com>
2026-09-08 17:31:12 +0700

    Remove backup directory from git

--- backup/src/Plugin.cpp
+++
@@ -1,6 +0,0 @@
-#include "Plugin.h"
-
-Plugin::Plugin(HMODULE hndl) : hModule(hndl) {
-    // การสร้าง PluginRender (ตัวแปร render ใน Plugin.h) 
-    // จะไปเรียก Constructor ของมัน ซึ่งจะทำการ Hook DirectX และจัดการ Load Screen ให้อัตโนมัติทันที
-}
